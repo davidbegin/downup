b6912ceffa70f19b5ca251593a639f3201efd779
David Begin <davidmichaelbe@gmail.com>
2015-08-06 21:26:09 -0700

    Spike of a keeping a custom header

--- lib/downup.rb
+++ lib/downup.rb
@@ -6,14 +6,16 @@ module Downup
   using Colors
 
   class Base
-    def initialize(options:, title: nil)
+    def initialize(options:, title: nil, header_proc: Proc.new)
       @options = options
-      @title = title
+      @title   = title
+      @header_proc  = header_proc
     end
 
     def prompt(position = 0)
       @selected_position = position_selector(position)
       system("clear")
+      header_proc.call
       print_title
       print_options
       print "\n> "
@@ -22,7 +24,7 @@ module Downup
 
     private
 
-    attr_reader :options, :title, :selected_position
+    attr_reader :options, :title, :selected_position, :header_proc
 
     def process_input(input)
       case input
